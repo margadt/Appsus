30cbe1f75911ecdd1c94862fd07cbb9d06917ce0
Margad T <spikkozor@gmail.com>
2019-12-25 11:32:19 +0200

    added route

--- js/Main.jsx
+++ js/Main.jsx
@@ -1,17 +1,34 @@
-import Home from './pages/Home.jsx'
+'use strict'
+
+const Router = ReactRouterDOM.HashRouter;
+const { Route, Switch } = ReactRouterDOM;
+const { createBrowserHistory } = History;
+const history = createBrowserHistory();
+
+
+import HomeApp from './pages/HomeApp.jsx'
+import EmailApp from './apps/email/pages/EmailApp.jsx'
+import KeepApp from './apps/missKeep/pages/KeepApp.jsx/index.js'
+
+
 class App extends React.Component {
 
     render() {
         return (
-            <main>
-                <Home />
-            </main>
+            <Router history={history}>
+                <NavBar></NavBar>
+                <Switch>
+                    <Route component={HomeApp} path='/' exact></Route>
+                    <Route component={EmailApp} path='/email' exact></Route>
+                    <Route component={KeepApp} path='/keep' exact></Route>
+                </Switch>
+            </Router>
         )
     }
 }
 
 
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.getElementById('root')
 )
---
+++ js/apps/missKeep/pages/KeepApp.jsx
@@ -0,0 +1,5 @@
+export default class KeepApp extends React.Component{
+    render(){
+        return <h1>KeepApp on</h1>
+    }
+}
